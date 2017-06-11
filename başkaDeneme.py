9b0a07ef556cf2f3de09bee46d446ec456aad1f6
ycy21 <aslanycan@gmail.com>
2017-06-11 21:18:11 +0300

    yazılar bold yapıldı

--- başkaDeneme.py
+++ başkaDeneme.py
@@ -29,44 +29,44 @@ class GUI (object):
         #Frame1
         self.frame1 = Frame(self.root)
         self.frame1.pack(side=TOP,fill=BOTH,expand=YES)
-        self.labelStart = Label(self.frame1, text="AIU Control Center", font=(12, 12), foreground="dark blue", background="light pink")
+        self.labelStart = Label(self.frame1, text="AIU Control Center", font=(12, 12,"bold"), foreground="dark blue")
         self.label5 = Label(self.frame1,text="                Minimum      ", font=(20,20), foreground="red")
         self.label6 = Label(self.frame1,text="Average   ", font=(20,20))
-        self.label7 = Label(self.frame1,text="  Maximum ", font=(20,20),foreground="green")
+        self.label7 = Label(self.frame1,text="  Maximum", font=(20,20),foreground="green")
         self.label0 = Label(self.frame1,textvariable=self.variable, font=(20,20), foreground="blue")   #variable yerine remChar gelecek
 
         #Frame2
         self.frame2 = Frame(self.root)
         self.frame2.pack(side=TOP,fill=BOTH,expand=YES)
-        self.label1 = Label(self.frame2, text="Voltage        ", font=(20, 20), foreground="blue", background="light blue")
-        self.label8 = Label(self.frame2,textvariable=self.variable,font=(14, 14), foreground="red", background="light blue")   #minVol
-        self.label9 = Label(self.frame2,textvariable=self.variable,font=(14, 14), background="light blue")
-        self.label10= Label(self.frame2,textvariable=self.variable,font=(14, 14),foreground="green", background="light blue")
+        self.label1 = Label(self.frame2, text="Voltage         ", font=(20, 20,"bold"), foreground="blue", background="light blue")
+        self.label8 = Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"), foreground="red", background="light blue")   #minVol
+        self.label9 = Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"), background="light blue")
+        self.label10= Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"),foreground="green", background="light blue")
 
         #Frame3
         self.frame3  = Frame(self.root)
         self.frame3.pack(side=TOP,fill=BOTH,expand=YES)
-        self.label2 = Label(self.frame3, text="Temperature", font=(20, 20), foreground="dark green", background="light green")
-        self.label11 = Label(self.frame3,textvariable=self.variable,font=(14, 14), foreground="red", background="light green")  #minTemp
-        self.label12 = Label(self.frame3,textvariable=self.variable, font=(14, 14), background="light green")
-        self.label13 = Label(self.frame3,textvariable=self.variable, font=(14, 14), foreground="green", background="light green")
+        self.label2 = Label(self.frame3, text="Temperature", font=(20, 20,"bold"), foreground="dark green", background="light green")
+        self.label11 = Label(self.frame3,textvariable=self.variable,font=(14, 14,"bold"), foreground="red", background="light green")  #minTemp
+        self.label12 = Label(self.frame3,textvariable=self.variable, font=(14, 14,"bold"), background="light green")
+        self.label13 = Label(self.frame3,textvariable=self.variable, font=(14, 14,"bold"), foreground="green", background="light green")
 
         #Frame4
         self.frame4 = Frame(self.root)
         self.frame4.pack(side=TOP,fill=BOTH,expand=YES)
-        self.label3 = Label(self.frame4, text="Current        ", font=(20, 20), foreground="light yellow", background="gold")
-        self.label17= Label(self.frame4,text="-", font=(14, 14), foreground="red", background="gold")
-        self.label14= Label(self.frame4,textvariable=self.variable,font=(14, 14), background="gold")  #curr
-        self.label18= Label(self.frame4,text="-", font=(14, 14), foreground="green", background="gold")
+        self.label3 = Label(self.frame4, text="Current         ", font=(20, 20,"bold"), foreground="light yellow", background="gold")
+        self.label17= Label(self.frame4,text="-", font=(14, 14,"bold"), foreground="red", background="gold")
+        self.label14= Label(self.frame4,textvariable=self.variable,font=(14, 14,"bold"), background="gold")  #curr
+        self.label18= Label(self.frame4,text="-", font=(14, 14,"bold"), foreground="green", background="gold")
 
 
         #Frame5
         self.frame5 = Frame(self.root)
         self.frame5.pack(side=TOP,fill=BOTH,expand=YES)
-        self.label4 = Label(self.frame5, text="Speed         ", font=(20, 20), foreground="purple", background="pink")
-        self.label16= Label(self.frame5,text="-", font=(14, 14), foreground="red", background="pink")
-        self.label15= Label(self.frame5,textvariable=self.variable, font=(14, 14), background="pink")  #speed
-        self.label19= Label(self.frame5,text="-", font=(14, 14), foreground="green", background="pink")
+        self.label4 = Label(self.frame5, text="Speed           ", font=(20, 20,"bold"), foreground="purple", background="pink")
+        self.label16= Label(self.frame5,text="-", font=(14, 14,"bold"), foreground="red", background="pink")
+        self.label15= Label(self.frame5,textvariable=self.variable, font=(14, 14,"bold"), background="pink")  #speed
+        self.label19= Label(self.frame5,text="-", font=(14, 14,"bold"), foreground="green", background="pink")
 
 
     def grid(self):
