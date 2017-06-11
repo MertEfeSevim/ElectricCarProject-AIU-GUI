2a72e4a44f7468bba108fb06a4e7c07962fcb51b
ycy21 <aslanycan@gmail.com>
2017-06-12 01:07:46 +0300

    total değer eklendi

--- başkaDeneme.py
+++ başkaDeneme.py
@@ -30,9 +30,10 @@ class GUI (object):
         self.frame1 = Frame(self.root)
         self.frame1.pack(side=TOP,fill=BOTH,expand=YES)
         self.labelStart = Label(self.frame1, text="AIU Control Center", font=(12, 12,"bold"), foreground="dark blue")
-        self.label5 = Label(self.frame1,text="                Minimum      ", font=(20,20), foreground="red")
-        self.label6 = Label(self.frame1,text="Average   ", font=(20,20))
-        self.label7 = Label(self.frame1,text="  Maximum", font=(20,20),foreground="green")
+        self.label5 = Label(self.frame1,text="         Minimum ", font=(20,20), foreground="red")
+        self.label6 = Label(self.frame1,text="Average ", font=(20,20))
+        self.label7 = Label(self.frame1,text="Maximum", font=(20,20),foreground="green")
+        self.labelTotal = Label(self.frame1, text="Total val.", font=(20, 20), foreground="gray")
         self.label0 = Label(self.frame1,textvariable=self.variable, font=(20,20), foreground="blue")   #variable yerine remChar gelecek
 
         #Frame2
@@ -42,6 +43,7 @@ class GUI (object):
         self.label8 = Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"), foreground="red", background="light blue")   #minVol
         self.label9 = Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"), background="light blue")
         self.label10= Label(self.frame2,textvariable=self.variable,font=(14, 14,"bold"),foreground="green", background="light blue")
+        self.labelTotalVal = Label(self.frame2, textvariable=(self.variable), font=(14, 14, "bold"), foreground="gray",background="light blue")
 
         #Frame3
         self.frame3  = Frame(self.root)
@@ -50,6 +52,7 @@ class GUI (object):
         self.label11 = Label(self.frame3,textvariable=self.variable,font=(14, 14,"bold"), foreground="red", background="light green")  #minTemp
         self.label12 = Label(self.frame3,textvariable=self.variable, font=(14, 14,"bold"), background="light green")
         self.label13 = Label(self.frame3,textvariable=self.variable, font=(14, 14,"bold"), foreground="green", background="light green")
+        self.labelSpace = Label(self.frame3, text="-", font=(14, 14, "bold"), foreground="gray", background="light green")
 
         #Frame4
         self.frame4 = Frame(self.root)
@@ -58,7 +61,7 @@ class GUI (object):
         self.label17= Label(self.frame4,text="-", font=(14, 14,"bold"), foreground="red", background="gold")
         self.label14= Label(self.frame4,textvariable=self.variable,font=(14, 14,"bold"), background="gold")  #curr
         self.label18= Label(self.frame4,text="-", font=(14, 14,"bold"), foreground="green", background="gold")
-
+        self.labelSpace1 = Label(self.frame4, text="-", font=(14, 14, "bold"), foreground="gray", background="gold")
 
         #Frame5
         self.frame5 = Frame(self.root)
@@ -67,7 +70,7 @@ class GUI (object):
         self.label16= Label(self.frame5,text="-", font=(14, 14,"bold"), foreground="red", background="pink")
         self.label15= Label(self.frame5,textvariable=self.variable, font=(14, 14,"bold"), background="pink")  #speed
         self.label19= Label(self.frame5,text="-", font=(14, 14,"bold"), foreground="green", background="pink")
-
+        self.labelSpace2 = Label(self.frame5, text="-", font=(14, 14, "bold"), foreground="gray", background="pink")
 
     def grid(self):
         self.label20.pack(side=LEFT,fill=BOTH,expand=YES)
@@ -81,23 +84,28 @@ class GUI (object):
         self.label5.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label6.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label7.pack(side=LEFT,fill=BOTH,expand=YES)
+        self.labelTotal.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label0.pack(side=LEFT,fill=BOTH,expand=YES)
 
         self.label8.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label9.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label10.pack(side=LEFT,fill=BOTH,expand=YES)
+        self.labelTotalVal.pack(side=LEFT, fill=BOTH, expand=YES)
 
         self.label11.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label12.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label13.pack(side=LEFT,fill=BOTH,expand=YES)
+        self.labelSpace.pack(side=LEFT,fill=BOTH,expand=YES)
 
         self.label17.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label14.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label18.pack(side=LEFT,fill=BOTH,expand=YES)
+        self.labelSpace1.pack(side=LEFT, fill=BOTH, expand=YES)
 
         self.label16.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label15.pack(side=LEFT,fill=BOTH,expand=YES)
         self.label19.pack(side=LEFT,fill=BOTH,expand=YES)
+        self.labelSpace2.pack(side=LEFT, fill=BOTH, expand=YES)
 
     def update_label(self):
         #Methodun benzerinin her değer için kullanılması gerekiyor.
