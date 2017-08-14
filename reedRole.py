d83faa2b2a51a7620af9aac808694406966d95f4
ycy21 <aslanycan@gmail.com>
2017-08-14 14:07:02 +0300

    bug fixes

--- reedRole.py
+++ reedRole.py
@@ -1,25 +1,34 @@
 import time
 import RPi.GPIO as io
+
 def measureSpeed():
     io.setmode(io.BCM)
     reed_pin = 22 #15 nolu pin
 
     io.setup(reed_pin, io.IN, pull_up_down=io.PUD_UP) # activate input with PullUp
 
-    temp=0
+    temp=False
     teker=0.000541
     values=[]
+
     for i in range(8):
+
         while True:
-            if io.input(reed_pin) and temp==0:
-                temp=1
+            if io.input(reed_pin) and temp == False:
+                temp=True
                 t0=time.time()
 
-            if io.input(reed_pin)==False and temp==1:
-                temp=0
+            if (io.input(reed_pin)== None or io.input(reed_pin)== False) and temp == True:
+                temp=False
                 t1 = time.time()
                 break
-        averagetimer=(t1-t0)/3600
-        values.append(teker/averagetimer)
-    result=sum(values)/len(values)
-    return result
+
+            else:
+                t0 = 1
+                t1 = 101
+                values.append(1)
+                break
+
+        values.append(teker/((t1-t0)/3600))
+
+    return sum(values)/len(values)
